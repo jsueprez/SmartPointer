b35c0b5d778ac06903d3a5df77fc82ac3e9ca815
jsueprez <ajperez502@gmail.com>
2022-10-16 18:49:40 +0200

    comparing benchmarking with chrono implementation

--- src/test/dummy_PerformanceTest.cpp
+++ src/test/dummy_PerformanceTest.cpp
@@ -1,17 +1,52 @@
 #include "benchmark/benchmark.h"
 #include "dummyLib.h"
 
-static void BM_dummySum(benchmark::State &state)
-{
-  DummyLib object;
-  int i1 = 2;
-  int i2 = 3;
+#include "dynamicArray.h"
+
+// Return a copy of arr with all of the values doubled
+DynamicArray<int> cloneArrayAndDouble(const DynamicArray<int> &arr) {
+  DynamicArray<int> dbl(arr.getLength());
+  for (auto i{0}; i < arr.getLength(); i++) {
+    dbl[i] = arr[i] * 2;
+  }
+  return dbl;
+}
+
+// Return a copy of arr with all of the values doubled
+DynamicArray_2<int> cloneArrayAndDouble_2(const DynamicArray_2<int> &arr) {
+  DynamicArray_2<int> dbl(arr.getLength());
+  for (auto i{0}; i < arr.getLength(); i++) {
+    dbl[i] = arr[i] * 2;
+  }
+  return dbl;
+}
+
+static void BM_DynamicArray_CopySemantics(benchmark::State &state) {
 
   // Perform setup here
-  for (auto _ : state)
-  {
+  for (auto _ : state) {
     // This code gets timed
-    object.dummySum(i1, i2);
+    DynamicArray<int> arr(1000000);
+    for (auto i{0}; i < arr.getLength(); i++) {
+      arr[i] = i;
+    }
+
+    arr = cloneArrayAndDouble(arr);
   }
 }
-BENCHMARK(BM_dummySum);
+BENCHMARK(BM_DynamicArray_CopySemantics);
+
+static void BM_DynamicArray_MoveSemantics(benchmark::State &state) {
+
+  // Perform setup here
+  for (auto _ : state) {
+    // This code gets timed
+    DynamicArray_2<int> arr(1000000);
+    for (auto i{0}; i < arr.getLength(); i++) {
+      arr[i] = i;
+    }
+
+    arr = cloneArrayAndDouble_2(arr);
+  }
+}
+BENCHMARK(BM_DynamicArray_MoveSemantics);
